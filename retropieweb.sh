c15f061f6c81d2e9f001eeb5b624b1cd4ecc5101
diandongshi <diandongshi@163.com>
2017-07-08 17:43:25 +0800

    Update retropieweb.sh

--- retropieweb.sh
+++ retropieweb.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+find /opt/retropie/configs/all/emulationstation/gamelists/ -name "gamelist.xml" |xargs -i chown root:root {}
 cd /home/pi/retropieweb
 git pull > /dev/null 2>&1
 python code.py 80
